c515e253fedea543cfd089c3897d006a5202ea1a
吴冰 <wubing@xuanit.com>
2025-01-24 17:03:51 +0800

    modify: 1. 修改Apple.cs和Tests.cs

---
+++ CoreLib/Base/Apple.cs
@@ -0,0 +1,26 @@
+﻿namespace CoreLib.Base;
+
+public class Apple
+{
+    private string Name { get; set; }
+    private AppleType AppleType { get; set; }
+
+    public Apple(string name, AppleType appleType)
+    {
+        Name = name;
+        AppleType = appleType;
+    }
+
+    public (string, AppleType) GetApple()
+    {
+        return (Name, AppleType);
+    }
+}
+
+public enum AppleType
+{
+    Unrip,          // 未成熟
+    ParitallyRipe,  // 部分成熟
+    Ripe,           // 完全成熟
+    Overripe        // 过熟
+}
--- UtilsTest/MetaModelTest/SnapshotTest/Tests.cs
+++ UtilsTest/MetaModelTest/SnapshotTest/Tests.cs
@@ -1,55 +1,48 @@
 ﻿using System.Runtime.CompilerServices;
+using CoreLib.Base;
 using CoreLib.Utils;
 
 namespace UtilsTest.MetaModelTest.SnapshotTest;
 
 public class Tests
 {
-    private static readonly VerifySettings Settings;
-    
-    static Tests()
-    {
-        Settings = new VerifySettings();
-        Settings.UseDirectory("snapshots");
-    }
-    
-    private readonly AppleService _sut = new();
-    
     [Fact]
-    public void Assert_apple_is_granny_smith()
-    {
-        var apple = _sut.GetApple();
-        Assert.Equal("Granny Smith", apple.Name);
-    }
-    
-    [Fact]
-    public Task Verify_apple_is_granny_smith()
+    public Task VerifyAppleData()
     {
         // arrange
-        var service = new AppleService();
-        // act 
-        var apple = service.GetApple(); 
+        var apple = new Apple("Good Apple", AppleType.ParitallyRipe);
         
+        // act
+        var data = apple.GetApple();
         
-        // verify    
-        return Verify(apple, Settings);
+        // verify
+        return Verify(data);
     }
+
+    //private readonly Apple _sut = new("Good Apple", AppleType.ParitallyRipe);
+    
+    // [Fact]
+    // public void Assert_apple_is_granny_smith()
+    // {
+    //     var apple = _sut.GetApple();
+    //     Assert.Equal("Granny Smith", apple.Name);
+    // }
 }
 
-public class StaticSettings
-{
-    [Fact]
-    public Task Test() =>
-        Verify("String to verify");
-}
-
-public static class StaticSettingsUsage
-{
-    [ModuleInitializer]
-    public static void Initialize()
-    {
-        VerifierSettings.AddScrubber(_ => _.Replace("String to verify", "new value"));
-    }
-}
+// public class StaticSettings
+// {
+//     [Fact]
+//     public Task Test() =>
+//         Verify("String to verify");
+// }
+//
+// public static class StaticSettingsUsage
+// {
+//     [ModuleInitializer]
+//     public static void Initialize()
+//     {
+//         VerifierSettings.AddScrubber(_ => _.Replace("String to verify", "new value"));
+//     }
+// }
 
 
