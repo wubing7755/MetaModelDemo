69ecf488399da45c300b271acfca5813e1c401de
吴冰 <wubing@xuanit.com>
2025-02-11 16:00:03 +0800

    add code through Cursor AI

---
+++ CodeAI/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeAI;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ CoreLib/Base/FileReadException.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace CoreLib.Base;
+
+[Serializable]
+public class CustomException : Exception
+{
+    public CustomException() : base() { }
+    
+    public CustomException(string message) : base(message) { }
+    
+    public CustomException(string message, Exception inner) : base(message, inner) { }
+    
+    protected CustomException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
