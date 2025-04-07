f279b291d8b589de3a7bceb8ec63411137cc8b45
wubing7755 <wubing7755@gmail.com>
2025-04-07 22:54:10 +0800

    add button

---
+++ SharedLibrary/Components/Button/Button.cs
@@ -0,0 +1,42 @@
+﻿
+
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.AspNetCore.Components.Web;
+using System.Diagnostics.CodeAnalysis;
+
+namespace SharedLibrary.Components;
+
+public class Button : ComponentBase, IDisposable
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter, NotNull]
+    public Func<MouseEventArgs, Task>? OnClick { get; set; }
+
+
+    protected override void BuildRenderTree(RenderTreeBuilder builder)
+    {
+        base.BuildRenderTree(builder);
+
+        int seq = 0;
+
+        builder.OpenElement(seq++, "div");
+        builder.AddAttribute(seq++, "style", "");
+
+        builder.OpenElement(seq++, "button");
+        builder.AddAttribute(seq++, "onclick", EventCallback.Factory.Create<MouseEventArgs>(this, OnClick));
+        builder.AddContent(seq++, ChildContent);
+        builder.CloseElement();
+
+        builder.CloseElement();
+
+    }
+
+
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+}
